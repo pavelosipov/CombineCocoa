23112ee930be3179996cf87369eaa9214a1183a8
Pavel Osipov <inbox@pavelosipov.net>
2022-09-05 18:23:53 +0300

    Restore CombineCocoa.h

---
+++ Sources/CombineCocoa/CombineCocoa.h
@@ -0,0 +1,13 @@
+//
+//  CombineCocoa.h
+//  CombineCocoa
+//
+//  Created by Joan Disho on 25/09/2019.
+//  Copyright © 2020 Combine Community. All rights reserved.
+//
+
+#import <CombineCocoa/ObjcDelegateProxy.h>
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double CombineCocoaVersionNumber;
+FOUNDATION_EXPORT const unsigned char CombineCocoaVersionString[];
